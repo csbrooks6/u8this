72cd3ccacb2c5fe8a78a13366602d38a0fd903e1
Christopher Brooks <chris@brooks6.com>
2014-02-15 12:29:12 -0600

    -Added User admin column (boolean) -Created new rake tasks to do basic user admin -- most importantly, to set the admin flag

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140124163756) do
+ActiveRecord::Schema.define(version: 20140215164513) do
 
   create_table "servings", force: true do |t|
     t.integer  "user_id"
@@ -35,22 +35,23 @@ ActiveRecord::Schema.define(version: 20140124163756) do
   add_index "user_sessions", ["updated_at"], name: "index_user_sessions_on_updated_at"
 
   create_table "users", force: true do |t|
-    t.string   "email",                              null: false
-    t.string   "crypted_password",                   null: false
-    t.string   "password_salt",                      null: false
-    t.string   "persistence_token",                  null: false
-    t.string   "single_access_token",                null: false
-    t.string   "perishable_token",                   null: false
-    t.integer  "login_count",         default: 0,    null: false
-    t.integer  "failed_login_count",  default: 0,    null: false
+    t.string   "email",                               null: false
+    t.string   "crypted_password",                    null: false
+    t.string   "password_salt",                       null: false
+    t.string   "persistence_token",                   null: false
+    t.string   "single_access_token",                 null: false
+    t.string   "perishable_token",                    null: false
+    t.integer  "login_count",         default: 0,     null: false
+    t.integer  "failed_login_count",  default: 0,     null: false
     t.datetime "last_request_at"
     t.datetime "current_login_at"
     t.datetime "last_login_at"
     t.string   "current_login_ip"
     t.string   "last_login_ip"
-    t.integer  "daily_calorie_goal",  default: 2000, null: false
+    t.integer  "daily_calorie_goal",  default: 2000,  null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",               default: false, null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
---
+++ lib/tasks/user_tasks.rake
@@ -0,0 +1,76 @@
+
+# TODO: Some tests would be nice!
+
+namespace :user do
+  desc "List basic information about all users"
+  task list: :environment do
+    users = User.all.order(:email)
+
+    format = "%20s|%11s|%25s|%5s|"
+    puts format % ["email", "login_count", "last_login_at", "admin"]
+    puts "-" * 80
+    users.each do |u|
+      puts format % [u.email, u.login_count, u.last_login_at, u.admin]
+    end
+  end
+
+  desc "Show detailed information about a user"
+  task :show, [:email] => [:environment] do |t, args|
+    if args.email.nil?
+      puts "USAGE: 'rake %s[bob@bob.com]'" % [t.name]
+      next
+    end
+
+    user = User.find_by(email: args.email)
+    if user.nil?
+      puts "%s: Can't find User with email '%s'" % [t.name, args.email]
+      next
+    end
+
+    User.attribute_names.each do |a|
+      puts "%20s: %s" % [a, user[a]]
+    end
+  end
+
+  desc "Delete a user"
+  task :delete, [:email] => [:environment] do |t, args|
+    if args.email.nil?
+      puts "USAGE: 'rake %s[bob@bob.com]'" % [t.name]
+      next
+    end
+
+    user = User.find_by(email: args.email)
+    if user.nil?
+      puts "%s: Can't find User with email '%s'" % [t.name, args.email]
+      next
+    end
+
+    if user.admin
+      puts "%s: Can't delete admin users (set_admin on %s to 0 first)" % [t.name, args.email]
+      next
+    end
+
+    user.destroy
+    puts "%s: Deleted user %s" % [t.name, args.email]
+  end
+
+
+  desc "Set the admin flag on a user"
+  task :set_admin, [:email, :admin] => [:environment] do |t, args|
+    if (args.email.nil? || args.admin.nil? || (!args.admin.in?(['0', '1'])) )
+      puts "USAGE: 'rake %s[bob@bob.com,1]'" % [t.name]
+      next
+    end
+
+    user = User.find_by(email: args.email)
+    if user.nil?
+      puts "%s: Can't find User with email '%s'" % [t.name, args.email]
+      next
+    end
+
+    user.admin = args.admin
+    user.save!
+  end
+
+
+end
